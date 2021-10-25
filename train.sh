a291932d2b42e42b1256644751eae64cbdf12178
justinbear <justin850509@gmail.com>
2021-10-25 21:35:25 +0800

    1026

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0,1 python3 train_net.py --config-file configs/COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml --num-gpus 2
+CUDA_VISIBLE_DEVICES=0,2 python3 train_net.py --config-file configs/COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml --num-gpus 2 --resume
